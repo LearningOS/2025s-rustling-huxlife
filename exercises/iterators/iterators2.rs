8142cfbf74061e9ee2f9d21afd575fe0edd671e6
hangxu <tsu321@163.com>
2025-03-28 23:48:07 +0800

    update2

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,8 +14,30 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let mut re = first.to_uppercase().collect::<String>();
+            re.push_str(c.as_str());
+            re
+        }
     }
+
+    // match c.next() {
+    //     Some(first) => {
+    //         first.to_uppercase()
+    //         .chain(c)
+    //         .collect()
+    //     },
+    //     None => {String::new()},
+    // }
+
+    // match c.next() {
+    //     Some(first) => {
+    //         format!("{}{}",
+    //         first.to_uppercase().collect::<String>(),
+    //         c.as_str())
+    //     },
+    //     None => {String::new()},
+    // }
 }
 
 // Step 2.
@@ -24,7 +45,35 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    // let mut data = words.into_iter();
+    // let mut new_data = vec![];
+    // while let Some(d) = data.next() {
+    //     let re = capitalize_first(*d);
+    //     new_data.push(re);
+    // }
+    // new_data
+
+    //2
+    // let mut new_data = vec![];
+    // let mut data = words.into_iter();
+    // data.for_each(|s|
+    //     new_data.push(capitalize_first(s))
+    // );
+    // new_data
+
+    ///3
+    // let mut new_data = Vec::with_capacity(words.len());
+    // for &ele in words {
+    //     new_data.push(capitalize_first(ele));
+    // }
+    // new_data
+
+    //4
+    words
+        .iter()// 转换为迭代器
+        .map(|&s| capitalize_first(s))// 模式匹配直接解引用
+        .collect()      
+
 }
 
 // Step 3.
@@ -32,7 +81,21 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+//     let mut data = words.into_iter();
+//     let mut result =  String::with_capacity(words.len());
+//     while let Some(&d) = data.next() {
+//         let re = capitalize_first(d);
+//         result.push_str(&re);
+//     }
+//    result
+
+    ///2
+    words.iter()
+    .map(|&s| capitalize_first(s))     // 先 map 转换类型
+    .fold(String::new(), |mut acc, s| {
+        acc.push_str(&s);              // 无中间字符串分配
+        acc
+    }) 
 }
 
 #[cfg(test)]
